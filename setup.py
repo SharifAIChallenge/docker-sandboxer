3efc1944505817dcfbf1cd21fc5fde6d5c42389f
Amir Keivan Mohtashami <akmohtashami97@gmail.com>
2016-02-12 14:04:16 +0330

    Added requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 from docker_sandboxer import VERSION
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements("requirements.txt")
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(name='docker-sandboxer',
       version=VERSION,
@@ -9,4 +13,5 @@ setup(name='docker-sandboxer',
       author_email='sharif.aichallenge@gmail.com',
       license='BSD',
       packages=find_packages(),
+      install_reqs=reqs,
       zip_safe=False)
